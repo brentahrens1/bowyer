777454c472f4353f1b6542cbf135035f045ad426
Brent Ahrens <cidida@gmail.com>
2022-03-08 16:50:55 -0800

    fix bugs add blocks

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,350 @@
-(()=>{var e,r={850:(e,r,t)=>{"use strict";t(370),t(508),t(740),t(846)},370:()=>{var e=document.querySelector(".target-norm"),r=document.querySelector(".target-clicked"),t=document.querySelector(".bowyer-nav__burger"),n=document.querySelector(".bowyer-nav__overlay");t.addEventListener("click",(function(){r.classList.toggle("clicked-open"),e.classList.toggle("norm-open"),n.classList.toggle("show")}))},846:()=>{var e=document.querySelectorAll(".carousel-inner__block"),r=Array.from(e),t=document.querySelector(".carousel-right"),n=document.querySelector(".carousel-left"),o=0,a=0;t.addEventListener("click",(function(){window.innerWidth>=800?(o=1,r.map((function(e){return e.style.transform="translateX(-100%)"}))):window.innerWidth<800&&(++o>=3&&(o=3),(a+=100)>=300&&(a=300),o>0&&o<=3&&r.map((function(e){return e.style.transform="translateX(-".concat(a,"%)")})))})),n.addEventListener("click",(function(){window.innerWidth>=800?1===o&&(r.map((function(e){return e.style.transform="translateX(0)"})),o=0):window.innerWidth<800&&0!==o&&(a-=100,r.map((function(e){return e.style.transform="translateX(-".concat(a,"%)")})),o--)}))},740:()=>{var e=document.querySelector(".bowyer-nav"),r=window.pageYOffset;window.onscroll=function(){var t=window.pageYOffset;0===window.pageYOffset||r>t?(e.style.top="0",e.style.backgroundColor="#F7F2EE"):(e.style.top="-100%",e.style.backgroundColor="transparent"),r=t},window.addEventListener("scroll",(function(){0===window.scrollY&&(e.style.top="0",e.style.backgroundColor="transparent")}))},508:()=>{var e=document.querySelectorAll(".section"),r=new IntersectionObserver((function(e,r){e.forEach((function(e){e.isIntersecting?e.target.classList.add("scroll-into-view"):e.target.classList.remove("scroll-into-view")}))}),{root:null,threshold:0,rootMargin:"-120px"});e.forEach((function(e){r.observe(e)}))},198:()=>{}},t={};function n(e){var o=t[e];if(void 0!==o)return o.exports;var a=t[e]={exports:{}};return r[e](a,a.exports,n),a.exports}n.m=r,e=[],n.O=(r,t,o,a)=>{if(!t){var l=1/0;for(u=0;u<e.length;u++){for(var[t,o,a]=e[u],c=!0,i=0;i<t.length;i++)(!1&a||l>=a)&&Object.keys(n.O).every((e=>n.O[e](t[i])))?t.splice(i--,1):(c=!1,a<l&&(l=a));if(c){e.splice(u--,1);var s=o();void 0!==s&&(r=s)}}return r}a=a||0;for(var u=e.length;u>0&&e[u-1][2]>a;u--)e[u]=e[u-1];e[u]=[t,o,a]},n.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return n.d(r,{a:r}),r},n.d=(e,r)=>{for(var t in r)n.o(r,t)&&!n.o(e,t)&&Object.defineProperty(e,t,{enumerable:!0,get:r[t]})},n.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r),(()=>{var e={260:0,367:0};n.O.j=r=>0===e[r];var r=(r,t)=>{var o,a,[l,c,i]=t,s=0;if(l.some((r=>0!==e[r]))){for(o in c)n.o(c,o)&&(n.m[o]=c[o]);if(i)var u=i(n)}for(r&&r(t);s<l.length;s++)a=l[s],n.o(e,a)&&e[a]&&e[a][0](),e[l[s]]=0;return n.O(u)},t=self.webpackChunkbowyer=self.webpackChunkbowyer||[];t.forEach(r.bind(null,0)),t.push=r.bind(null,t.push.bind(t))})(),n.O(void 0,[367],(()=>n(850)));var o=n.O(void 0,[367],(()=>n(198)));o=n.O(o)})();
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scripts/app.js":
+/*!****************************!*\
+  !*** ./src/scripts/app.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _burger__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./burger */ "./src/scripts/burger.js");
+/* harmony import */ var _burger__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_burger__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _sectionObserver__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./sectionObserver */ "./src/scripts/sectionObserver.js");
+/* harmony import */ var _sectionObserver__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_sectionObserver__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./nav */ "./src/scripts/nav.js");
+/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_nav__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _carousel__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./carousel */ "./src/scripts/carousel.js");
+/* harmony import */ var _carousel__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_carousel__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+
+/***/ }),
+
+/***/ "./src/scripts/burger.js":
+/*!*******************************!*\
+  !*** ./src/scripts/burger.js ***!
+  \*******************************/
+/***/ (() => {
+
+var targetNorm = document.querySelector('.target-norm');
+var targetClicked = document.querySelector('.target-clicked');
+var burger = document.querySelector('.bowyer-nav__burger');
+var overlay = document.querySelector('.bowyer-nav__overlay');
+burger.addEventListener('click', function () {
+  targetClicked.classList.toggle("clicked-open");
+  targetNorm.classList.toggle("norm-open");
+  overlay.classList.toggle("show");
+});
+
+/***/ }),
+
+/***/ "./src/scripts/carousel.js":
+/*!*********************************!*\
+  !*** ./src/scripts/carousel.js ***!
+  \*********************************/
+/***/ (() => {
+
+var carouselBlock = document.querySelectorAll('.carousel-inner__block');
+var itemArray = Array.from(carouselBlock);
+var btnRight = document.querySelector('.carousel-right');
+var btnLeft = document.querySelector('.carousel-left');
+var count = 0;
+var transformMove = 0;
+
+function moveLeft() {
+  if (window.innerWidth >= 800) {
+    if (count === 1) {
+      itemArray.map(function (s) {
+        return s.style.transform = "translateX(0)";
+      });
+      count = 0;
+    }
+  } else if (window.innerWidth < 800) {
+    if (count !== 0) {
+      transformMove = transformMove - 100;
+      itemArray.map(function (s) {
+        return s.style.transform = "translateX(-".concat(transformMove, "%)");
+      });
+      count--;
+    }
+  }
+}
+
+function moveRight() {
+  if (window.innerWidth >= 800) {
+    count = 1;
+    itemArray.map(function (s) {
+      return s.style.transform = "translateX(-100%)";
+    });
+  } else if (window.innerWidth < 800) {
+    count++;
+
+    if (count >= 3) {
+      count = 3;
+    }
+
+    transformMove = transformMove + 100;
+
+    if (transformMove >= 300) {
+      transformMove = 300;
+    }
+
+    if (count > 0 && count <= 3) {
+      itemArray.map(function (s) {
+        return s.style.transform = "translateX(-".concat(transformMove, "%)");
+      });
+    }
+  }
+}
+
+btnRight.addEventListener('click', moveRight);
+btnLeft.addEventListener('click', moveLeft);
+
+/***/ }),
+
+/***/ "./src/scripts/nav.js":
+/*!****************************!*\
+  !*** ./src/scripts/nav.js ***!
+  \****************************/
+/***/ (() => {
+
+var nav = document.querySelector('.bowyer-nav');
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+
+  if (window.pageYOffset === 0) {
+    nav.style.top = "0";
+    nav.style.backgroundColor = "#F7F2EE";
+  } else if (prevScrollpos > currentScrollPos) {
+    nav.style.top = "0";
+    nav.style.backgroundColor = "#F7F2EE";
+  } else {
+    nav.style.top = "-100%";
+    nav.style.backgroundColor = "transparent";
+  }
+
+  prevScrollpos = currentScrollPos;
+};
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY === 0) {
+    nav.style.top = "0";
+    nav.style.backgroundColor = "transparent";
+  }
+});
+
+/***/ }),
+
+/***/ "./src/scripts/sectionObserver.js":
+/*!****************************************!*\
+  !*** ./src/scripts/sectionObserver.js ***!
+  \****************************************/
+/***/ (() => {
+
+var sections = document.querySelectorAll('.section');
+var options = {
+  root: null,
+  threshold: 0,
+  rootMargin: "-120px"
+};
+var observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      entry.target.classList.remove("scroll-into-view");
+    } else {
+      entry.target.classList.add("scroll-into-view");
+    }
+  });
+}, options);
+sections.forEach(function (section) {
+  observer.observe(section);
+});
+
+/***/ }),
+
+/***/ "./src/styles/app.scss":
+/*!*****************************!*\
+  !*** ./src/styles/app.scss ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/app": 0,
+/******/ 			"dist/app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkIds[i]] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunkbowyer"] = self["webpackChunkbowyer"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["dist/app"], () => (__webpack_require__("./src/scripts/app.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["dist/app"], () => (__webpack_require__("./src/styles/app.scss")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
--- single-project.php
+++ single-project.php
@@ -25,8 +25,8 @@
                                 </div>
                             </div>
                         <?php elseif(get_row_layout() == 'image_and_text_block'):
-                            $header = get_sub_field('heading');
-                            $headerColor = get_sub_field('heading_color');
+                            $heading = get_sub_field('heading');
+                            $headingColor = get_sub_field('heading_color');
                             $bodyText = get_sub_field('body_text');
                             $bodyTextColor = get_sub_field('body_text_color');
                             $imageRight = get_sub_field('image_right');
@@ -131,6 +131,59 @@
                                     <img src="<?php echo $image6 ?>" alt="" />
                                 </div>
                             </div>
+                            <?php elseif (get_row_layout() == 'carousel'):
+                            $blocks = get_sub_field('carousel_blocks');
+                        ?>  
+                        <div class="carousel">
+                            <div class="carousel-inner">
+                                <?php foreach($blocks as $block): ?>
+                                    <div class="carousel-inner__block" style="background-color: <?php echo $block['background_color'];?>; border-top: <?php echo $block['bt'];?>; border-right: <?php echo $block['br'];?>; border-bottom: <?php echo $block['bb'];?>; border-left: <?php echo $block['bl'];?>;">
+                                        <div class="carousel-image-container">
+                                            <?php if ($block['image']): ?>
+                                                <img src="<?php echo $block['image'];?>" />
+                                            <?php endif; ?>
+                                            <?php if ($block['hovered_image']): ?>
+                                                <img class="hovered-image" src="<?php echo $block['hovered_image'];?>" />
+                                            <?php endif; ?>
+                                        </div>
+                                        <div class="carousel-content">
+                                            <?php if ($block['title']): ?>
+                                                <h2 style="color: <?php echo $block['title_color']; ?>"><?php echo $block['title'];?></h2>
+                                            <?php endif; ?>
+                                            <?php if ($block['description']): ?>
+                                                <p style="color: <?php echo $block['description_color']; ?>"><?php echo $block['description'];?></p>
+                                            <?php endif; ?>
+                                        </div>                            
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                            <button class="carousel-right">Right</button>
+                            <button class="carousel-left">Left</button>
+                        </div>
+                        <?php elseif (get_row_layout() == 'image_hover_grid'):
+                            $blocks = get_sub_field('image_block');
+                        ?>  
+                        <div class="format-container">
+                            <div class="format">
+                                <div class="format-inner">
+                                    <?php foreach($blocks as $block): ?>
+                                        <div class="format-inner__block" style="background-color: <?php echo $block['background_color'];?>; border-top: <?php echo $block['bt'];?>; border-right: <?php echo $block['br'];?>; border-bottom: <?php echo $block['bb'];?>; border-left: <?php echo $block['bl'];?>;">
+                                            <?php if ($block['heading']): ?>
+                                                <h2 style="color: <?php echo $block['heading_color']; ?>"><?php echo $block['heading'];?></h2>
+                                            <?php endif; ?>
+                                            <?php if ($block['cta']): ?>
+                                                <p style="color: <?php echo $block['cta_color']; ?>"><?php echo $block['cta'];?>
+                                                    <span><img src="<?php echo get_theme_file_uri('/images/right-arrow.png'); ?>" alt="" /></span>
+                                                </p>
+                                            <?php endif; ?>
+                                            <?php if ($block['image']): ?>
+                                                <img src="<?php echo $block['image'];?>" />
+                                            <?php endif; ?>
+                                        </div>
+                                    <?php endforeach; ?>
+                                </div>
+                            </div>
+                        </div>
                         <?php endif; ?>
                     <?php endwhile; ?>
                 <?php endif; ?>
